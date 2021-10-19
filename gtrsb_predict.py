1a26b20f2a9c33a61240c23223cb2c43fa8bbc45
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-10-18 22:52:27 -0500

    Add code comments

--- gtrsb_predict.py
+++ gtrsb_predict.py
@@ -4,8 +4,8 @@ import numpy as np
 # How to use the pretrained and saved model to do standalone prediction of images
 def predict_with_model(model, img_path):
 
-    image = tf.io.read_file(img_path)
-    image = tf.image.decode_png(image, channels=3)
+    image = tf.io.read_file(img_path) # read image file at path
+    image = tf.image.decode_png(image, channels=3) # decode file as png and specific number of channels = 3
     image = tf.image.convert_image_dtype(image, dtype=tf.float32) # also scales the values by 255 to fall between 0 and 1
     image = tf.image.resize(image, [60,60]) # resize the image, shape is 60,60,3
     image = tf.expand_dims(image, axis=0) # shape is (1, 60, 60, 3) 
